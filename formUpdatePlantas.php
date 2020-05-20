6aac5318c546dafec1d504ad6d666ef7279358f2
Le Yo <leyo@si.com>
2020-05-20 19:53:43 +0200

    minor changes

--- formUpdatePlantas.php
+++ formUpdatePlantas.php
@@ -212,19 +212,19 @@
   <script src="//malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
 
 <!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="vendors/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendors/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendors/bootstrap/js/popper.js"></script>
+	<script src="vendors/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendors/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendors/daterangepicker/moment.min.js"></script>
+	<script src="vendors/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="vendors/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
   <script src="js/main.js"></script>
   <script src="js/mainPortada.js"></script>
