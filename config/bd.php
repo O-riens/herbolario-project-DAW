10b43617e13015a59e6de136fbf3b69bd309b48c
Le Yo <leyo@si.com>
2020-05-22 20:09:32 +0200

    database

--- config/bd.php
+++ config/bd.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'den1.mysql6.gear.host');
+define('DB_SERVER', 'den1.mysql2.gear.host');
 define('DB_USERNAME', 'herbario');
 define('DB_PASSWORD', 'Informatica1*');
 define('DB_NAME', 'herbario');
